4ee8b30b9f8f67453c17c52205ec519e72534f4c
LuizAugustoBR <luiz.ab.rocha97@gmail.com>
2024-11-08 17:13:50 -0300

    Implementação do banco de dados e add de entidade e id ao models

--- src/main/java/com/social/social/models/Social.java
+++ src/main/java/com/social/social/models/Social.java
@@ -1,42 +1,62 @@
 package com.social.social.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "social")
 public class Social {
 
-	private String nome;
-	private String local;
-	private String data;
-	private String horario;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	public String getNome() {
-		return nome;
-	}
+    private String nome;
+    private String local;
+    private String data;
+    private String horario;
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
 
-	public String getLocal() {
-		return local;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setLocal(String local) {
-		this.local = local;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getData() {
-		return data;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setData(String data) {
-		this.data = data;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public String getHorario() {
-		return horario;
-	}
+    public String getLocal() {
+        return local;
+    }
 
-	public void setHorario(String horario) {
-		this.horario = horario;
-	}
+    public void setLocal(String local) {
+        this.local = local;
+    }
 
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
 }
